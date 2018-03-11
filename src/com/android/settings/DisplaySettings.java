479efd8ed6746bc71419527d46efc7c7f79f1081
LuK1337 <priv.luk@gmail.com>
2018-03-11 11:38:52 +0100

    Settings: Add high touch sensitivity and touchscreen hovering toggles
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Mar 26 00:06:11 2019 +0000
    
        DisplaySettings: Hide high touch sensitivity from search if needed
    
         * Fixes: https://gitlab.com/LineageOS/issues/android/issues/260
    
        Change-Id: I98ce66dec9801c36da5a2facd6b033895b7963f0
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Mar 26 00:20:37 2019 +0000
    
        LanguageAndInputSettings: Hide touch hovering from search if needed
    
        Change-Id: I5d53946262919ff67be5e777654af2171ffd2a44
    
    Change-Id: Ib8712d2d0c0fe4396dbea7c7fc130e0c253d3207

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -44,6 +44,8 @@ import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,6 +56,7 @@ public class DisplaySettings extends DashboardFragment {
     public static final String KEY_PROXIMITY_ON_WAKE = "proximity_on_wake";
 
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
+    private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
 
     private IntentFilter mIntentFilter;
     private static FontPickerPreferenceController mFontPickerPreference;
@@ -153,6 +156,11 @@ public class DisplaySettings extends DashboardFragment {
                     if (!context.getResources().getBoolean(
                             com.android.internal.R.bool.config_proximityCheckOnWake)) {
                         keys.add(KEY_PROXIMITY_ON_WAKE);
+                    }
+                    LineageHardwareManager hardware = LineageHardwareManager.getInstance(context);
+                    if (!hardware.isSupported(
+                            LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
+                        keys.add(KEY_HIGH_TOUCH_SENSITIVITY);
                     }
                     return keys;
                 }
