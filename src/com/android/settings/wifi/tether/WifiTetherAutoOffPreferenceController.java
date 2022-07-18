NAMELESS-AOSP-OPENSOURCE/packages_apps_Settings

SoftAp: add support for setting shutdown timeout
Change-Id: I80aebe3ca8e13e3dc5c503d5a8527282e96e486a
Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>

src/ink/kscope/settings/wifi/tether/WifiTetherAutoOffPreferenceController.java
@@ -1,0 +1,86 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ink.kscope.settings.wifi.tether;
+
+import android.content.Context;
+import android.net.wifi.SoftApConfiguration;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import ink.kscope.settings.wifi.tether.preference.WifiTetherAutoOffPreference;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class WifiTetherAutoOffPreferenceController extends BasePreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final int MAX_TIMEOUT_MINUTES = 90;
+
+    private final WifiManager mWifiManager;
+
+    private WifiTetherAutoOffPreference mPreference;
+
+    public WifiTetherAutoOffPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+        mWifiManager = context.getSystemService(WifiManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        mPreference.setMax(MAX_TIMEOUT_MINUTES);
+        updateDisplay();
+    }
+
+    private int getAutoOffTimeout() {
+        SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
+        final boolean settingsOn = softApConfiguration.isAutoShutdownEnabled();
+        return settingsOn ? (int) (softApConfiguration.getShutdownTimeoutMillis() / 60 / 1000) : 0;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int timeout = (int) newValue;
+        SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
+        SoftApConfiguration newSoftApConfiguration =
+                new SoftApConfiguration.Builder(softApConfiguration)
+                        .setAutoShutdownEnabled(timeout > 0)
+                        .setShutdownTimeoutMillis((long) timeout * 60 * 1000)
+                        .build();
+        return mWifiManager.setSoftApConfiguration(newSoftApConfiguration);
+    }
+
+    public void updateConfig(SoftApConfiguration.Builder builder) {
+        if (builder == null) return;
+        final int timeout = getAutoOffTimeout();
+        builder.setAutoShutdownEnabled(timeout > 0)
+                .setShutdownTimeoutMillis((long) timeout * 60 * 1000);
+    }
+
+    public void updateDisplay() {
+        mPreference.setValue(getAutoOffTimeout(), false);
+    }
+}

src/com/android/settings/wifi/tether/WifiTetherSettings.java
@@ -44,6 +44,7 @@ import com.android.settingslib.TetherUtil;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
 import ink.kscope.settings.wifi.tether.WifiTetherHiddenSsidPreferenceController;
+import ink.kscope.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,6 +75,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
     private WifiTetherSecurityPreferenceController mSecurityPreferenceController;
     private WifiTetherHiddenSsidPreferenceController mHiddenSsidPrefController;
     private WifiTetherApBandPreferenceController mApBandPrefController;
+    private WifiTetherAutoOffPreferenceController mAutoOffPrefController;
 
     private WifiManager mWifiManager;
     private boolean mRestartWifiApAfterConfigChange;
@@ -121,6 +123,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         mPasswordPreferenceController = use(WifiTetherPasswordPreferenceController.class);
         mApBandPrefController = use(WifiTetherApBandPreferenceController.class);
         mHiddenSsidPrefController = use(WifiTetherHiddenSsidPreferenceController.class);
+        mAutoOffPrefController = use(WifiTetherAutoOffPreferenceController.class);
     }
 
     @Override
@@ -221,6 +224,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         }
         configBuilder.setHiddenSsid(mHiddenSsidPrefController.isHiddenSsidEnabled());
         mApBandPrefController.setupBands(configBuilder);
+        mAutoOffPrefController.updateConfig(configBuilder);
         return configBuilder.build();
     }
 
@@ -235,6 +239,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         use(WifiTetherPasswordPreferenceController.class).updateDisplay();
         use(WifiTetherApBandPreferenceController.class).updateDisplay();
         use(WifiTetherHiddenSsidPreferenceController.class).updateDisplay();
+        use(WifiTetherAutoOffPreferenceController.class).updateDisplay();
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =

src/ink/kscope/settings/wifi/tether/preference/WifiTetherAutoOffPreference.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ink.kscope.settings.wifi.tether.preference;
+
+import android.content.Context;
+import android.widget.SeekBar;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.settings.R;
+import com.android.settings.SeekBarDialogPreference;
+
+public class WifiTetherAutoOffPreference extends SeekBarDialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private Context mContext;
+    private SeekBar mSeekBar;
+    private int mValue;
+    private int mMax;
+
+    public WifiTetherAutoOffPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        setText(getSummaryForTimeout(progress));
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mSeekBar = getSeekBar(view);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mSeekBar.setMax(mMax);
+        mSeekBar.setProgress(mValue);
+    }
+
+    public String getSummaryForTimeout(int timeout) {
+        if (timeout == 0) {
+            return mContext.getResources().getString(R.string.wifi_hotspot_auto_off_timeout_never);
+        } else {
+            return mContext.getResources().getQuantityString(R.plurals.wifi_hotspot_auto_off_timeout, timeout, timeout);
+        }
+    }
+
+    public void setMax(int max) {
+        mMax = max;
+    }
+
+    public void setValue(int value, boolean callListener) {
+        mValue = value;
+        String summary = getSummaryForTimeout(value);
+        setSummary(summary);
+        setText(summary);
+        if (callListener) callChangeListener(value);
+    }
+
+    public int getValue() {
+        return mValue;
+    }
+
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            setValue(mSeekBar.getProgress(), true);
+        }
+    }
+}

tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSettingsTest.java
@@ -45,6 +45,8 @@ import com.android.settings.testutils.FakeFeatureFactory;
 import com.android.settings.testutils.shadow.ShadowFragment;
 import com.android.settings.testutils.shadow.ShadowWifiManager;
 
+import ink.kscope.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;

Kuruin/android_packages_apps_Settings-1

src/com/android/settings/wifi/tether/WifiTetherAutoOffPreferenceController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2022 Project Kaleidoscope
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,16 +19,22 @@ package com.android.settings.wifi.tether;
 import android.content.Context;
 import android.net.wifi.SoftApConfiguration;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.wifi.tether.preference.WifiTetherAutoOffPreference;
 
 public class WifiTetherAutoOffPreferenceController extends BasePreferenceController implements
         Preference.OnPreferenceChangeListener {
 
+    private static final int MAX_TIMEOUT_MINUTES = 90;
+
     private final WifiManager mWifiManager;
+
+    private WifiTetherAutoOffPreference mPreference;
 
     public WifiTetherAutoOffPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -41,21 +47,39 @@ public class WifiTetherAutoOffPreferenceController extends BasePreferenceControl
     }
 
     @Override
-    public void updateState(Preference preference) {
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        mPreference.setMax(MAX_TIMEOUT_MINUTES);
+        updateDisplay();
+    }
+
+    private int getAutoOffTimeout() {
         SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
         final boolean settingsOn = softApConfiguration.isAutoShutdownEnabled();
-
-        ((SwitchPreference) preference).setChecked(settingsOn);
+        return settingsOn ? (int) (softApConfiguration.getShutdownTimeoutMillis() / 60 / 1000) : 0;
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final boolean settingsOn = (Boolean) newValue;
+        int timeout = (int) newValue;
         SoftApConfiguration softApConfiguration = mWifiManager.getSoftApConfiguration();
         SoftApConfiguration newSoftApConfiguration =
                 new SoftApConfiguration.Builder(softApConfiguration)
-                        .setAutoShutdownEnabled(settingsOn)
+                        .setAutoShutdownEnabled(timeout > 0)
+                        .setShutdownTimeoutMillis((long) timeout * 60 * 1000)
                         .build();
         return mWifiManager.setSoftApConfiguration(newSoftApConfiguration);
+    }
+
+    public void updateConfig(SoftApConfiguration.Builder builder) {
+        if (builder == null) return;
+        final int timeout = getAutoOffTimeout();
+        builder.setAutoShutdownEnabled(timeout > 0)
+                .setShutdownTimeoutMillis((long) timeout * 60 * 1000);
+    }
+
+    public void updateDisplay() {
+        mPreference.setValue(getAutoOffTimeout(), false);
     }
 }

src/com/android/settings/wifi/tether/WifiTetherSettings.java
@@ -40,6 +40,7 @@ import com.android.settings.core.FeatureFlags;
 import com.android.settings.dashboard.RestrictedDashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.SettingsMainSwitchBar;
+import com.android.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherHiddenSsidPreferenceController;
 import com.android.settingslib.TetherUtil;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -74,6 +75,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
     private WifiTetherSecurityPreferenceController mSecurityPreferenceController;
     private WifiTetherHiddenSsidPreferenceController mHiddenSsidPrefController;
     private WifiTetherApBandPreferenceController mApBandPrefController;
+    private WifiTetherAutoOffPreferenceController mAutoOffPrefController;
 
     private WifiManager mWifiManager;
     private boolean mRestartWifiApAfterConfigChange;
@@ -121,6 +123,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         mPasswordPreferenceController = use(WifiTetherPasswordPreferenceController.class);
         mApBandPrefController = use(WifiTetherApBandPreferenceController.class);
         mHiddenSsidPrefController = use(WifiTetherHiddenSsidPreferenceController.class);
+        mAutoOffPrefController = use(WifiTetherAutoOffPreferenceController.class);
     }
 
     @Override
@@ -221,6 +224,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         }
         configBuilder.setHiddenSsid(mHiddenSsidPrefController.isHiddenSsidEnabled());
         mApBandPrefController.setupBands(configBuilder);
+        mAutoOffPrefController.updateConfig(configBuilder);
         return configBuilder.build();
     }
 
@@ -235,6 +239,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         use(WifiTetherPasswordPreferenceController.class).updateDisplay();
         use(WifiTetherApBandPreferenceController.class).updateDisplay();
         use(WifiTetherHiddenSsidPreferenceController.class).updateDisplay();
+        use(WifiTetherAutoOffPreferenceController.class).updateDisplay();
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =

src/com/android/settings/wifi/tether/preference/WifiTetherAutoOffPreference.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.wifi.tether.preference;
+
+import android.content.Context;
+import android.widget.SeekBar;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.android.settings.R;
+import com.android.settings.SeekBarDialogPreference;
+
+public class WifiTetherAutoOffPreference extends SeekBarDialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private Context mContext;
+    private SeekBar mSeekBar;
+    private int mValue;
+    private int mMax;
+
+    public WifiTetherAutoOffPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        setText(getSummaryForTimeout(progress));
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+
+        mSeekBar = getSeekBar(view);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mSeekBar.setMax(mMax);
+        mSeekBar.setProgress(mValue);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            setValue(mSeekBar.getProgress(), true);
+        }
+    }
+
+    public String getSummaryForTimeout(int timeout) {
+        if (timeout == 0) {
+            return mContext.getResources().getString(
+                R.string.wifi_hotspot_auto_off_timeout_never);
+        } else {
+            return mContext.getResources().getQuantityString(
+                R.plurals.wifi_hotspot_auto_off_timeout, timeout, timeout);
+        }
+    }
+
+    public void setMax(int max) {
+        mMax = max;
+    }
+
+    public void setValue(int value, boolean callListener) {
+        mValue = value;
+        String summary = getSummaryForTimeout(value);
+        setSummary(summary);
+        setText(summary);
+        if (callListener) callChangeListener(value);
+    }
+
+    public int getValue() {
+        return mValue;
+    }
+}

tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSettingsTest.java
@@ -44,6 +44,7 @@ import com.android.settings.core.FeatureFlags;
 import com.android.settings.testutils.FakeFeatureFactory;
 import com.android.settings.testutils.shadow.ShadowFragment;
 import com.android.settings.testutils.shadow.ShadowWifiManager;
+import com.android.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
 
 import org.junit.Before;
 import org.junit.Ignore;